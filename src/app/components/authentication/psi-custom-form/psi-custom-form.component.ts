b69421f36bd9f3dc2ca0d11630d867d69e416554
priyank-parkstreet <priyankp@parkstreet.com>
2024-09-17 12:53:42 +0530

    Login Functionality

--- src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
+++ src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
@@ -8,33 +8,51 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class PsiCustomFormComponent implements OnInit {
   loginForm = new FormGroup({
-    userName : new FormControl('',[Validators.required]),
-    password : new FormControl('',[
+    userName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(6)
     ]),
   });
   formSubmitted: boolean;
-  bothInvalid : boolean;
-  
+  bothInvalid: boolean;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onSubmit(){
-    debugger
-    this.formSubmitted = true;
+  // onSubmit(){
+  // this.formSubmitted = true;
+  // console.log(this.loginForm.value.password !=='' && this.loginForm.value.userName !=='' )
 
-  if (this.loginForm.invalid) {
-    // this.bothInvalid = true;
-    // (this.loginForm.controls.userName.status === 'INVALID' && this.loginForm.controls.password.status === 'INVALID'){
+  // if (this.loginForm.invalid) {
 
+  // return;
+  // }
 
-    // }
-    // this.loginForm.markAllAsTouched();
-    return;
+  onSubmit(form) {
+    if (form.valid) {
+      // this.usSpinnerService.spin('app-loader');
+      // this.formConfig.serverValidation = '';
+      // let reqObj: any = {
+      //   username: this.user.email,
+      //   password: this.user.password,
+      //   token: this.token,
+      //   email_verification_token: this.emailVerificationToken,
+      //   skip2fa: false
+      // };
+      // if (this.commonService.getURLParameter('compeId')) {
+      //   reqObj.is_competition_req = true;
+      //   reqObj.competition_id = this.commonService.getURLParameter('compeId');
+      // }
+      // this.loginService.userLogin(reqObj).then((response) => {
+      //   this.processResponseAfterLogin(response);
+      //   this.usSpinnerService.stop('app-loader');
+      // });
+    }
   }
-  }
-
 }
+
+
+
