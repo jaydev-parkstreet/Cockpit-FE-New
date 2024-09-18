b96ee1dfd9146424ceed1912546205f30a0c0d96
priyank-parkstreet <priyankp@parkstreet.com>
2024-09-18 16:29:54 +0530

    Design changes

--- src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
+++ src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PsiCustomFormService } from './psi-custom-form.service';
 import { environment } from 'src/environments/environment';
