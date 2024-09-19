c2ea87acd6fa0d0dc437fab1cca34c9fe6bb2f2e
anuradha-parkstreet <aaldar@parkstreet.com>
2024-09-19 18:13:30 +0530

    POC-Cockpit

--- src/app/shared/components/header-tool/header-tool.component.ts
+++ src/app/shared/components/header-tool/header-tool.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-tool',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderToolComponent implements OnInit {
 
+  @Input() titleText: string;
+  @Input() titleIcon: string;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
