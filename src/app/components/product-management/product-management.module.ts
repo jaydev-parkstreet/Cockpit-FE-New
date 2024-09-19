2ff6d8fede9c455c43ad85faaad3851ee8bda9ce
anuradha-parkstreet <aaldar@parkstreet.com>
2024-09-19 13:06:31 +0530

    POC-shared components

--- src/app/components/product-management/product-management.module.ts
+++ src/app/components/product-management/product-management.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductManagementComponent } from './product-management.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ProductManagementComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: []
 })
