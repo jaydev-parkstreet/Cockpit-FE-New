4214ab31d7efce232c4a0c749405f6fcf4168b64
priyank-parkstreet <priyankp@parkstreet.com>
2024-10-08 14:25:08 +0530

    PEQ-2745-POC

--- src/app/components/product-management/product-management.component.ts
+++ src/app/components/product-management/product-management.component.ts
@@ -23,96 +23,68 @@ export class ProductManagementComponent implements OnInit {
   isLoadingSummaryData: boolean = false;
   productToolSummary: any;
   productToolCardSummary: any;
-  hasMoreRecords:boolean;
-  isLoading:boolean;
-  busy:boolean;
+  hasMoreRecords: boolean;
+  isLoading: boolean;
+  busy: boolean;
   isGridSortApplied: boolean;
   params: any;
-  // rowData:any;
-  colDefs:any;
+  colDefs: any;
+  columnDefs: any = [];
+  rowData: any[] = [];
+  filters: any;
+  permissionObj: any;
+  isSorting: boolean;
+  scrollDisabled: boolean;
 
-  // rowData: any[] = [
-  //   { make: 'Toyota', model: 'Celica', price: 35000 },
-  //   { make: 'Ford', model: 'Mondeo', price: 32000 },
-  //   { make: 'Porsche', model: 'Boxster', price: 72000 }
-  // ];
-
-  // columnDefs: ColDef[] = [
-  //   { field: 'make' },
-  //   { field: 'model' },
-  //   { field: 'price' }
-  // ];
-
-  columnDefs: any =[];
-  rowData: any[] =[];
-
-  constructor(private productManagementService:ProductManagementService,
-    private authService:AuthService,private router:Router) { }
+  constructor(private productManagementService: ProductManagementService,
+    private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-
-    //Angular 10 appproach------------
-
-
-    // --------------------------
     // this.getSummaryData();
     // this.getDropdown();
-    // this.gridOptions = 'hello';
+    // this.loadGridData();
+    this.reportRequestObj = {};
+    this.selectedRowCount = 0;
+    // this.selectedCardRowCount = 0;
+    this.productToolCardSummary = [];
+    // this.selectedAll = false;
+    this.busy = true;
+    this.filters = {};
+    this.permissionObj = {};
+    this.isSorting = false;
+    this.scrollDisabled = false;
+    // this.queryParam = this.commonService.$location.search();
+    // this.openNotePopup = false;
+    // this.defaultState = this.productToolService.getDefaultState()[0].id;
+    // this.reportRequestObj = {
+    //     page: 1,
+    //     pageSize: 25,
+    //     sort: 'status',
+    //     order: 'desc',
+    //     active_status: [this.defaultState]
+    // };
+    // this.summaryTopBarConfig = this.productToolService.getSummaryTopBarConfig();
+    // this.summaryTopBarConfig.actions = this.productToolService.getDefaultActions(this.reportRequestObj,
+    //     this.permissions.permissions.Create);
+    // this.statusObj = this.productToolService.getStatusObject();
     this.initGridOptions();
-    this.loadGridData();
-  this.reportRequestObj = {};
-  this.selectedRowCount = 0;
-  // this.selectedCardRowCount = 0;
-  this.productToolCardSummary = [];
-  // this.selectedAll = false;
-  this.busy = true;
-  // this.filters = {};
-  // this.permissionObj = {};
-  // this.isSorting = false;
-  // this.scrollDisabled = false;
-  // this.queryParam = this.commonService.$location.search();
-  // this.openNotePopup = false;
-  // this.defaultState = this.productToolService.getDefaultState()[0].id;
-  // this.reportRequestObj = {
-  //     page: 1,
-  //     pageSize: 25,
-  //     sort: 'status',
-  //     order: 'desc',
-  //     active_status: [this.defaultState]
-  // };
-  // this.summaryTopBarConfig = this.productToolService.getSummaryTopBarConfig();
-  // this.summaryTopBarConfig.actions = this.productToolService.getDefaultActions(this.reportRequestObj,
-      // this.permissions.permissions.Create);
-  // this.statusObj = this.productToolService.getStatusObject();
-  // this.initGridOptions();
-  // this.timeout(() => {
-  //     document.body.click();
-  // }, 500);
-  
   }
 
 
   initGridOptions() {
     this.gridOptions = this.productManagementService.getGridOption();
-    // this.setDataSourceAgGrid()
-    console.log(this.productManagementService.getGridOption());
-    console.log(this.productManagementService.getSummaryTableHeaderConfig());
+    this.gridOptions.onGridReady = () => {
+      this.setDataSourceAgGrid();
+    };
   }
 
-  async loadGridData() {
-    await this.getSummaryData();
-    // this.setDataSourceAgGrid()
-    this.updateGridData();
-
-  }
-
-  updateGridData(){
-    if (this.summaryResponse) {
-      this.rowData = this.summaryResponse;
-    }
-  }
-
-  async getSummaryData(){
+  /**
+     * Function to get summary data
+     *
+     * @createdDate 03-08-2024
+     * @author PSI-Enhancements
+    */
+  async getSummaryData() {
     const token = localStorage.getItem('authToken');
     const summaryData = {
       "page": 1,
@@ -121,16 +93,18 @@ export class ProductManagementComponent implements OnInit {
       "order": "asc"
     }
     try {
-      const response:any = await this.productManagementService.getSummary(summaryData,token);
+      const response: any = await this.productManagementService.getSummary(summaryData, token);
       this.hasMoreRecords = response.data.length === 25;
       this.summaryResponse = response.data;
-      // this.processResponseData(response,params);
+      this.processResponseData(response, this.params);
     }
     catch (error) {
       console.error("Error fetching summary:", error);
     }
   }
 
+
+  //Function to logout
   logout () {
     this.authService.logout();
     this.router.navigate(['/login']);
@@ -143,59 +117,57 @@ export class ProductManagementComponent implements OnInit {
   //   })
   // }
 
-  // initGridOptions(){
-  //   this.gridOptions = this.productManagementService.getGridOption();
-  // }
 
-//   setDataSourceAgGrid() {
-//     if (!this.isLoadingSummaryData) {
-//         this.selectedAllRows = false;
-//         this.selectedRowCount = 0;
-//         this.selectedRows = [];
-//         this.selectedCardRows = [];
-//         this.mixType = false;
-//         // angular.element('.checkbox_select_all').prop('checked', false);
-//         this.isLoadingSummaryData = true;
-//         // this.gridOptions.api.hideOverlay();
-//         this.reportRequestObj.page = 1;
-//         this.productToolSummary = [];
-//         this.productToolCardSummary = [];
-//         this.hasMoreRecords = true;
-//         this.isLoading = false;
-//         this.busy = true;
-//         // if (this.isGridSortApplied !== true) {
-//         //     this.summaryTopBarConfig.actions = this.productToolService.getDefaultActions(this.reportRequestObj,
-//         //         this.permissions.permissions.Create);
-//         // }
-//         this.isGridSortApplied = false;
-//         const dataSource = {
-//             rowCount: null,
-            
-//             getRows: (params) => {
-//               console.log('paraaaaa',params)
-//                 // this.params = params;
-//                 if (!this.isLoading && (this.reportRequestObj.page === 1 ||
-//                     (params.startRow >= this.productToolSummary.length)) && this.hasMoreRecords) {
-//                     this.isLoading = true;
-
-//                     this.getSummaryData(params);
-                    
-//                 } 
-//                 else {
-//                     this.successCallback(params);
-//                 }
-//                 // if (!this.filtersList) {
-//                     // this.setFilterList();
-//                 // }
-//             }
-//         };
-//         console.log(dataSource);
-//         this.gridOptions.datasource = dataSource;
-//     }
-// }
+  /**
+     * Function to call api and set ag-grid dataSource object
+     *
+     * @createdDate 03-10-2024
+     * @author PSI-Enhancements
+    */
+  setDataSourceAgGrid() {
+    if (!this.isLoadingSummaryData) {
+      this.selectedAllRows = false;
+      this.selectedRowCount = 0;
+      this.selectedRows = [];
+      this.selectedCardRows = [];
+      this.mixType = false;
+      // angular.element('.checkbox_select_all').prop('checked', false);
+      this.isLoadingSummaryData = true;
+      // this.gridOptions.api.hideOverlay();
+      this.reportRequestObj.page = 1;
+      this.productToolSummary = [];
+      this.productToolCardSummary = [];
+      this.hasMoreRecords = true;
+      this.isLoading = false;
+      this.busy = true;
+      // if (this.isGridSortApplied !== true) {
+      //     this.summaryTopBarConfig.actions = this.productToolService.getDefaultActions(this.reportRequestObj,
+      //         this.permissions.permissions.Create);
+      // }
+      this.isGridSortApplied = false;
+      const dataSource = {
+        rowCount: null,
+        getRows: (params) => {
+          this.params = params;
+          if (!this.isLoading && (this.reportRequestObj.page === 1 ||
+            (params.startRow >= this.productToolSummary.length)) && this.hasMoreRecords) {
+            this.isLoading = true;
+            this.getSummaryData();
+          }
+          else {
+            this.successCallback(params);
+          }
+          // if (!this.filtersList) {
+          // this.setFilterList();
+          // }
+        }
+      };
+      this.gridOptions.api.setDatasource(dataSource);
+    }
+  }
 
 
-    /**
+  /**
      * Function to return display rows object
      *
      * @createdDate 03-10-2024
@@ -203,47 +175,58 @@ export class ProductManagementComponent implements OnInit {
      * @param array data
      * @param number startRow
      * @param number endRow
-     */
-    // getDisplayRows(data, startRow, endRow) {
-    //   debugger
-    //   const rowsThisPage = data.slice(startRow, endRow);
-    //   let lastRow = -1;
-    //   if (!this.hasMoreRecords) {
-    //   lastRow = data.length;
-    //   }
-    //   return { rowsThisPage: rowsThisPage, lastRow: lastRow };
-    // }
+    */
+  getDisplayRows (data, startRow, endRow) {
+    const rowsThisPage = data.slice(startRow, endRow);
+    let lastRow = -1;
+    if (!this.hasMoreRecords) {
+      lastRow = data.length;
+    }
+    return { rowsThisPage: rowsThisPage, lastRow: lastRow };
+  }
 
-  //   processResponseData(response, params) {
-  //     console.log("inside");
-  //     if (response.data.length > 0) {
-  //         // response.data.forEach(row => {
-  //         //     row.iconPaymentClass = row.is_active ? '' : 'fas fa-ban u-mt1 u-ml2 neutral-light';
-  //         // });
-  //         this.productToolSummary = [...this.productToolSummary || [], ...response.data];
-  //         this.productToolCardSummary = [...this.productToolCardSummary || [], ...response.data];
-  //         this.reportRequestObj.page++;
-  //         // this.successCallback(params);
-  //     } 
-  //     else if (this.reportRequestObj.page === 1) {
-  //         params.successCallback(this.productToolSummary, 0);
-  //         this.gridOptions.api.showLoadingOverlay();
-  //         this.productToolCardSummary = [];
-  //     }
-  //      else {
-  //         this.successCallback(params);
-  //     }
-  //     this.isLoading = false;
-  //     this.busy = false;
-  //     this.isLoadingSummaryData = false;
-  // }
 
-  // successCallback(params): void {
-  //   console.log('1',params);
-  //   setTimeout(() => {
-  //       const returnObj = this.getDisplayRows(this.productToolSummary, params.startRow, params.endRow);
-  //       params.successCallback(returnObj.rowsThisPage, returnObj.lastRow);
-  //   }, 500);
-  // }
+  /**
+     * Function to process response data.
+     * @createdDate 08-10-2024
+     * @author PSI-Enhancements
+     * @param object response
+     * @param object params
+    */
+  processResponseData (response, params) {
+    if (response.data.length > 0) {
+      // response.data.forEach(row => {
+      //     row.iconPaymentClass = row.is_active ? '' : 'fas fa-ban u-mt1 u-ml2 neutral-light';
+      // });
+      this.productToolSummary = [...this.productToolSummary || [], ...response.data];
+      this.productToolCardSummary = [...this.productToolCardSummary || [], ...response.data];
+      this.reportRequestObj.page++;
+      this.successCallback(params);
+    }
+    else if (this.reportRequestObj.page === 1) {
+      params.successCallback(this.productToolSummary, 0);
+      this.gridOptions.api.showLoadingOverlay();
+      this.productToolCardSummary = [];
+    }
+    else {
+      this.successCallback(params);
+    }
+    this.isLoading = false;
+    this.busy = false;
+    this.isLoadingSummaryData = false;
+  }
+
+  /**
+     * Function to call success callback for ag-grid.
+     * @createdDate 08-10-2024
+     * @author PSI-Enhancements
+     * @param object params
+    */
+  successCallback(params): void {
+    setTimeout(() => {
+      const returnObj = this.getDisplayRows(this.productToolSummary, params.startRow, params.endRow);
+      params.successCallback(returnObj.rowsThisPage, returnObj.lastRow);
+    }, 500);
+  }
 
 }
