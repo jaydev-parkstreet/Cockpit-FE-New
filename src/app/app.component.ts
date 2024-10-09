1b5e1b30fb47432c3e40ba2778479483d0449448
priyank-parkstreet <priyankp@parkstreet.com>
2024-10-09 13:25:53 +0530

    PEQ-2745 issues resolved summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'COCKPIT-FE';
+
+  constructor(private spinner: NgxSpinnerService) {
+
+  }
+  ngOnInit() {
+    // this.spinner.show();
+    // setTimeout(() => {
+    //   this.spinner.hide();
+
+    // }, 8000);
+
+
+  }
 }
