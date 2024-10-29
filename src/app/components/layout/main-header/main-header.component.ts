336e98bbace2dcf27dfbc46eef44542d6ff1855d
anuradha-parkstreet <aaldar@parkstreet.com>
2024-10-29 18:08:59 +0530

    PEQ-2745 decoupling

--- src/app/components/layout/main-header/main-header.component.ts
+++ src/app/components/layout/main-header/main-header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { AuthService } from './../../../components/authentication/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-header.component.scss']
 })
 export class MainHeaderComponent implements OnInit {
-
-  constructor() { }
+  isDropdownOpen = false;
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ) {}
 
   ngOnInit(): void {
   }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (this.isDropdownOpen && !target.closest('.dropdown')) {
+      this.closeDropdown();
+    }
+  }
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
 
 }
