64f06c25b1770826752ddeda339050ad8a65dd6e
maglodiya-parkstreet <mohmmadali.aglodiya@growexx.com>
2024-10-30 13:29:29 +0530

    PEQ-2745-main

--- src/app/components/product-management/product-management.component.ts
+++ src/app/components/product-management/product-management.component.ts
@@ -107,9 +107,8 @@ export class ProductManagementComponent implements OnInit {
         this.reportRequestObj.order = 'asc';
       }
       this.productToolSummary = [];
-      this.isLoadingSummaryData = true;
       this.setDataSourceAgGrid();
-	  };
+	};
     this.gridOptions.onGridReady = () => {
       this.setDataSourceAgGrid();
     };
@@ -281,7 +280,6 @@ export class ProductManagementComponent implements OnInit {
   }
 
   applyFilters(selectedFilters: any) {
-	console.log(selectedFilters);
 	this.filtermodal = Object.keys(selectedFilters).reduce((acc, key) => {
         acc[key] = selectedFilters[key].map((item: any) => item.id); 
         return acc;
@@ -310,6 +308,7 @@ export class ProductManagementComponent implements OnInit {
 
   universalSearch (text) {
     this.reportRequestObj.universal_search = text;
+	this.productToolSummary = [];
     this.setDataSourceAgGrid();
   }
 	excelExport() {
@@ -345,7 +344,11 @@ export class ProductManagementComponent implements OnInit {
     }
     this.selectAllFlag = checked;
     this.selectedRowCount = this.selectedAllRows ? this.productToolSummary.length : 0;
-	this.isProductSelected = !this.isProductSelected;
+	if (this.selectAllFlag) {
+		this.isProductSelected = true
+	} else {
+		this.isProductSelected = false
+	}
     this.gridOptions.api.redrawRows();
   }
 
@@ -363,7 +366,11 @@ export class ProductManagementComponent implements OnInit {
     } else {
         this.selectAllFlag = true;
     }
-	this.isProductSelected = !this.isProductSelected;
+	if (this.selectAllFlag) {
+		this.isProductSelected = true
+	} else {
+		this.isProductSelected = false
+	}
     this.gridOptions.api.redrawRows();
   }
 }
