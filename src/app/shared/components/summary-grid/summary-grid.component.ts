3b0fc5780a993f2fe10f89fdc40375346acebbfe
Kris Narola <knarola@parksteet.com>
2024-11-07 19:41:33 +0530

    PEQ-2745 checkbox-position-bug-fixing

--- src/app/shared/components/summary-grid/summary-grid.component.ts
+++ src/app/shared/components/summary-grid/summary-grid.component.ts
@@ -1,19 +1,26 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild, ElementRef} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-summary-grid',
   templateUrl: './summary-grid.component.html',
   styleUrls: ['./summary-grid.component.scss']
 })
-export class SummaryGridComponent implements OnChanges {
+export class SummaryGridComponent implements OnChanges, OnInit {
 
   @Input() gridOptions: any;
   @Input() selectAllOption: boolean;
   @Input() checkAll: boolean;
   @Output() selectAllChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
   @ViewChild('checkboxSelectAll') checkboxSelectAll!: ElementRef;
+  @ViewChild('checkboxContainer') checkboxContainer!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
+
+    ngOnInit(): void {
+      if(this.selectAllOption) {
+        this.gridOptions.onBodyScroll = this.onGridBodyScroll.bind(this);      
+      }
+    }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['checkAll']) {
@@ -31,4 +38,12 @@ export class SummaryGridComponent implements OnChanges {
         this.checkboxSelectAll.nativeElement.checked = isChecked;
     }
 }
+
+    onGridBodyScroll(event: any) { 
+      if (event.direction === 'horizontal' && event.left >= 20) {
+        this.renderer.setStyle(this.checkboxContainer.nativeElement, 'display', 'none');
+      } else if (event.direction === 'horizontal') {
+        this.renderer.setStyle(this.checkboxContainer.nativeElement, 'display', 'block');
+      }
+    }
 }
