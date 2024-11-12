6362ea981f494b3a671b3052d585c4a961b36a3e
harpreet-parkstreet <hkaur@parkstreet.com>
2024-11-12 15:02:32 +0530

    fixprefillvalue

--- src/app/components/product-management/product-add/product-add.component.ts
+++ src/app/components/product-management/product-add/product-add.component.ts
@@ -219,14 +219,17 @@ export class ProductAddComponent implements OnInit {
     this.productmanagementService.getDetails(productId).subscribe((productData) => {
         this.renderConditionalFields(productData.prod_type)
          this.productId = productData.product_id;
-        if (this.duplicate) {
-          delete productData.product_id;
-        }
-        if (!this.duplicate) {
-            this.uniqueId = productData.id;
-        }
-        this.modelFormat = this.productmanagementService.formatModelProductTool(productData, this.filtersList, this.subBrandProducts, this.edit, this.duplicate, this.uniqueId,this.productId);
-        this.prefillForm(productData);
+         setTimeout(() => {
+          if (this.duplicate) {
+            delete productData.product_id;
+          }
+          if (!this.duplicate) {
+              this.uniqueId = productData.id;
+          }
+          this.modelFormat = this.productmanagementService.formatModelProductTool(productData, this.filtersList, this.subBrandProducts, this.edit, this.duplicate, this.uniqueId,this.productId);
+          this.prefillForm(productData);          
+         }, 100);
+       
     });
   }
   prefillForm(productData: any): void { 
