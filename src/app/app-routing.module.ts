4bb9317451574330768f2244830cbf0417a909ff
harpreet-parkstreet <hkaur@parkstreet.com>
2024-11-14 16:58:24 +0530

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './components/authentication/login-page/login-page.component';
 import { MiddleComponent } from './components/authentication/middle/middle.component';
-import { ProductManagementComponent } from './components/product-management/product-management.component';
 import { AuthGuard } from './components/authentication/auth.guard';
-import { ProductAddComponent } from './components/product-management/product-add/product-add.component';
-import { ProductManagementDetailsComponent } from './components/product-management/product-management-details/product-management-details.component';
-import { ApplicationComponent } from './components/layout/application/application.component';
-import { FilterListResolver } from './core/resolver/filter-list.resolver';
 
 const routes: Routes = [
   {
