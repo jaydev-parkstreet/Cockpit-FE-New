089515bf4297ce61d4bcaeb743188636e830aee1
maglodiya-parkstreet <maglodiya@parkstreet.com>
2025-03-07 13:04:28 +0530

    PEQ-2745

--- src/app/components/product-management/product-add/product-add.component.ts
+++ src/app/components/product-management/product-add/product-add.component.ts
@@ -99,7 +99,6 @@ export class ProductAddComponent implements OnInit {
      * @author psi-enhancement
      */
     prefillForm(productData: any): void {
-        console.log(productData);
         this.productForm.patchValue({
             client_id: this.getDropDownArrayByIds(this.crudFiltersList?.clients, productData.client_id, 'client_id'),
             description: productData.description,
@@ -124,7 +123,7 @@ export class ProductAddComponent implements OnInit {
             manufactured_location_address: productData?.manufactured_location_address,
             upc_code: productData.upc_code,
             scc_code: productData.scc_code,
-            system_id: productData.system_id,
+            system_id: productData.supplier_ref_id,
             cola_ttb_id: productData.cola_ttb_id,
             nabca_code: productData.nabca_code,
             unimerc_code: productData?.unimerc_code,
