ce31915d2c44eefc0b7c818b1e69ae8523a8da33
priyank-parkstreet <priyankp@parkstreet.com>
2025-03-13 12:41:06 +0530

    POC -Sidebar design

--- src/app/components/layout/sidebar-menu/sidebar-menu.component.ts
+++ src/app/components/layout/sidebar-menu/sidebar-menu.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 // import { log } from 'console';
 import { environment } from 'src/environments/environment';
 import { AuthService } from '../../authentication/auth.service';
@@ -15,15 +15,38 @@ export class SidebarMenuComponent implements OnInit {
   isDropdownVisible : boolean = false;
   currentUserData: any;
   oldCockpit: string = environment.oldCockpit;
+  // @ViewChild('submenuItem') submenuItem: any;
 
   constructor(
     private http:HttpClient,
-    private authService: AuthService
+    private authService: AuthService,
+    private renderer: Renderer2,
   ) { }
 
   ngOnInit(): void {
     this.currentUserData = this.authService.getUserData()
     this.sidebarItems();
+  }
+
+  showSubmenu(event: MouseEvent, anchorElement: HTMLElement) {
+    const submenuItem = anchorElement.parentElement as HTMLElement;
+    if (!submenuItem) return;
+
+    const submenuElement = submenuItem.querySelector('.submenu-item') as HTMLElement;
+    if (!submenuElement) return;
+    const parentRect = submenuItem.getBoundingClientRect();
+    this.renderer.setStyle(submenuElement, 'top', `${parentRect.top}px`);
+    this.renderer.setStyle(submenuElement, 'display', 'block');
+  }
+
+  hideSubmenu(event: MouseEvent, anchorElement: HTMLElement) {
+
+    const submenuItem = anchorElement.parentElement as HTMLElement;
+    if (!submenuItem) return;
+
+    const submenuElement = submenuItem.querySelector('.submenu-item') as HTMLElement;
+    if (!submenuElement) return;
+    this.renderer.setStyle(submenuElement, 'display', 'none');
   }
 
   toggleIcon (menu:any) {
