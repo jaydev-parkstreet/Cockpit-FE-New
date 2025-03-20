d3cc562f233f239f730b22d048e5b2b6f7c48234
maglodiya-parkstreet <maglodiya@parkstreet.com>
2025-03-20 17:43:01 +0530

    PEQ-2745 AG Grid Tooltip issue Detail sync issue

--- src/app/components/product-management/product-management.service.ts
+++ src/app/components/product-management/product-management.service.ts
@@ -109,6 +109,7 @@ export class ProductManagementService {
             infiniteInitialRowCount: 1,
             maxConcurrentDatasourceRequests: 2,
             enableServerSideSorting: true,
+            suppressRowTransform: true,
             defaultColDef: {
             width: 200,
             sortable: true,
@@ -178,7 +179,7 @@ export class ProductManagementService {
     renderDash(params) {
         if (params.value) {
             return `<div class="text-ellipsis"><span>${params.value}</span>
-                    <span class="add-tooltip">${params.value}</span></div>`;
+                    <span class="add-tooltip tooltip-text">${params.value}</span></div>`;
         }
         return '--';
     }
