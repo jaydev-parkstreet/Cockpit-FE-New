5ccf0bd8f5d2fa7ed9d11f6dd9f0ae810b779200
jsatpathy-parkstreet <jsatpathy@parkstreet.com>
2025-03-20 15:23:35 +0530

    reusable file upload

--- src/app/components/product-management/mass-upload-excel-modal/mass-upload-excel-modal.component.ts
+++ src/app/components/product-management/mass-upload-excel-modal/mass-upload-excel-modal.component.ts
@@ -37,29 +37,28 @@ export class MassUploadExcelModalComponent extends SimpleModalComponent<massUplo
         const maxSize = 10 * 1024 * 1024;
         const validFiles: File[] = [];
         if (files.length > 1) {
-          alert('Only 1 files are allowed to be uploaded.');
-          return;
+            alert('Only 1 files are allowed to be uploaded.');
+            return;
         }
         for (let i = 0; i < files.length; i++) {
-          const file = files[i];
-          const fileName = file.name;
-          const fileSize = file.size;
-          const fileExtension = fileName.split('.').pop()?.toLowerCase();
-    
-          if (fileSize > maxSize) {
-            alert(`${fileName} is too large! Please upload file up to 10 MB.`)
-            return;
-          }
-    
-          if (!fileExtension || !allowedExtensions.includes(fileExtension)) {
-            alert(`Only ${allowedExtensions.join(', ')} are allowed to be uploaded.`)
-            return;
-          }
-          validFiles.push(file);
+            const file = files[i];
+            const fileName = file.name;
+            const fileSize = file.size;
+            const fileExtension = fileName.split('.').pop()?.toLowerCase();
+
+            if (fileSize > maxSize) {
+                alert(`${fileName} is too large! Please upload file up to 10 MB.`)
+                return;
+            }
+
+            if (!fileExtension || !allowedExtensions.includes(fileExtension)) {
+                alert(`Only ${allowedExtensions.join(', ')} are allowed to be uploaded.`)
+                return;
+            }
+            validFiles.push(file);
         }
         this.selectedFiles = validFiles;
-        // this.selectedFileCount = this.selectedFiles.length;
-      }
+    }
 
 }
 
