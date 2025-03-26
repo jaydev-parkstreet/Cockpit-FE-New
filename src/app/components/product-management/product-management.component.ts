95ed07e8701a3d429f221e0d64541a8674372623
Kris Narola <knarola@parksteet.com>
2025-03-26 16:57:43 +0530

    PEQ-2745 Summary-Grid-Sync-Button

--- src/app/components/product-management/product-management.component.ts
+++ src/app/components/product-management/product-management.component.ts
@@ -9,11 +9,12 @@ import { environment } from 'src/environments/environment';
 import { SimpleModalService } from 'ngx-simple-modal';
 import { CmpAttachmentModalComponent } from 'src/app/shared/components/cmp-attachment-modal/cmp-attachment-modal.component';
 import { ConfirmationModalComponent } from '../organism/confirmation-modal/confirmation-modal.component';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-management',
   templateUrl: './product-management.component.html',
-  styleUrls: ['./product-management.component.scss'],
+  styleUrls: ['./product-management.component.scss']
 })
 
 export class ProductManagementComponent implements OnInit {
@@ -47,6 +48,7 @@ export class ProductManagementComponent implements OnInit {
   FileSaver: any;
   downloading: boolean;
   filtermodal: any;
+  private timerSubscription!: Subscription;
 
   constructor(
     private productManagementService: ProductManagementService,
@@ -113,6 +115,16 @@ export class ProductManagementComponent implements OnInit {
           this.selectCheckBox(params);
       }else if (params.event.target.className === 'far fa-file show-attachment-modal' || params.event.target.className === 'fas fa-file show-attachment-modal') {
         this.openAttachmentListPopup(params.data.product_id);
+      } else if(params.colDef.cellRenderer === 'idRender' && (params.event.target.className === 're-sync')) {
+        this.productManagementService.syncOrder(params.value).subscribe( (response: any) => {
+          if(!response.hasError) {
+            params.data.ns_status = 2;
+            this.timerSubscription = interval(30000).subscribe(()=> {
+                this.getSyncStatusDetails(params);
+            });
+            this.gridOptions.api.redrawRows();
+          }
+        });
       }
     };
     this.gridOptions.onCellMouseOver = (params) => {
@@ -491,4 +503,31 @@ showAttachment(multiple:any, entityIds:any, attachments:any) {
     );
     
   };
+
+    /**
+     * Fetches the synchronization status details for a specific product.
+     *
+     * @param {Object} params - The grid row parameters containing product data.
+     * @returns {void}
+     * @author PSI-Enhancement
+     */
+    getSyncStatusDetails(params) {
+        this.productManagementService.getSyncStatusDetails(params.data.product_id).subscribe( (response: any) => {
+            if(!response.hasError && response.data) {
+                params.data.ns_status = response.data.status;
+                if(response.data.status === 1) {
+                    this.commonService.showToastV2Message(true, 'Sync Successful', null, 'success');
+                } else if(response.data.status === 3) {
+                    this.commonService.showToastV2Message(true, 'Sync Failed');
+                }
+                this.gridOptions.api.redrawRows();
+                this.timerSubscription.unsubscribe();
+            } else {
+                this.timerSubscription.unsubscribe();
+                params.data.ns_status = 3;
+                this.gridOptions.api.redrawRows();
+                this.commonService.showToastV2Message(true, 'Save Failed');
+            }
+        });
+    }
 }
--- src/app/components/product-management/product-management.service.ts
+++ src/app/components/product-management/product-management.service.ts
@@ -64,7 +64,8 @@ export class ProductManagementService {
             minWidth: 150,
             width: 150,
             field: 'product_id',
-            cellRenderer: 'idRender'
+            cellRenderer: 'idRender',
+            cellClass: 'tooltip-cell'
         },
         { headerName: 'Product Description', minWidth: 75, width: 193, field: 'description', cellRenderer: 'dashRenderer', cellClass: 'tooltip-cell' },
         { headerName: 'Supplier', minWidth: 75, width: 115, field: 'client_name', cellRenderer: 'dashRenderer', cellClass: 'tooltip-cell' },
@@ -190,11 +191,55 @@ export class ProductManagementService {
      * @author psi-enhancements
      */
     renderId(params) {
-      if (params.value) {
-        return `<a target="_blank" style="color: black;text-decoration: none;" onmouseover="this.style.textDecoration='underline'"
-                onmouseout="this.style.textDecoration='none'" href="product-management/${params.value}">${params.value}</a>`;
-      }
-      return '-';
+        if (params.value === null || params.value === '---' || params.value === '-') {
+            return '--';
+        } else if (params.data && params.data.product_id) {
+            let fbStatusToolTip = '';
+            if (params.data.ns_status === 2) {
+                fbStatusToolTip = `
+                <i class="fas fa-clock sync-pending u-base-warning">
+                    <div class="tooltip-content_">
+                        <div class="tooltip-text_"><span class="sync-heading">Sync Status:</span> In Queue</div>
+                        <i></i>
+                    </div>
+                </i>`;
+            } else if (params.data.ns_status === 3 && params.data.status === "Pending") {
+                fbStatusToolTip = `
+                <i class="fas fa-exclamation-circle sync-failed u-base-error">
+                    <div class="tooltip-content_pending_status">
+                        <div class="tooltip-text_">
+                            <span class="fail">Sync Status:<span class="fail-msg"> Failed</span></span>
+                        </div>
+                        <i></i>
+                    </div>
+                </i>`;
+            }
+            else if (params.data.ns_status === 3 && params.data.status !== "Pending") {
+                fbStatusToolTip = `
+                <i class="fas fa-exclamation-circle sync-failed u-base-error">
+                    <div class="tooltip-content_">
+                        <div class="tooltip-text_ u-pg-g-1">
+                            <span class="fail">Sync Status:<span class="fail-msg"> Failed</span></span>
+                            <span class="re-sync">Re-Sync</span>
+                        </div>
+                        <i></i>
+                    </div>
+                </i>`;
+            }
+            return `
+                <div class="text-ellipsis">
+                    <a target="_blank" style="color: black; text-decoration: none;" 
+                        onmouseover="this.style.textDecoration='underline'"
+                        onmouseout="this.style.textDecoration='none'" 
+                        href="product-management/${params.value}">
+                        ${params.value}
+                    </a>
+                    ${fbStatusToolTip}
+                    <span class="add-tooltip">${params.value}</span>
+                </div>`;
+        } else {
+            return '<i class="fas fa-circle-notch fa-spin fa-2x fa-fw"></i>';
+        }
     }
   
     /**
@@ -547,6 +592,30 @@ export class ProductManagementService {
             'Content-Type': ''
           })
         });
-      }
-      
+    }
+
+    /**
+     * Syncs the product order with NS.
+     * @param productId
+     * @returns An Observable containing the response from the server.
+     * @author PSI-Enhancement
+     */
+    syncOrder(productId) {
+        let params = {
+            'productId' : productId
+        };
+        return this.http
+            .post(environment.apiUrl + AppRoutes.PRODUCT_TOOL.NS_SYNC, params);
+    }
+
+    /**
+     * Fetches the sync status details for a product
+     * @param id
+     * @returns An observable containing the sync status details
+     * @author PSI-Enhancement
+     */
+    getSyncStatusDetails(id) {
+        return this.http
+          .get(environment.apiUrl + AppRoutes.PRODUCT_TOOL.NS_SYNC_STATUS + id);
+    }
 }
