1e7f7815e550d9f0b17ffe2ba5739cc301030d3a
jsatpathy-parkstreet <jsatpathy@parkstreet.com>
2025-03-28 17:55:08 +0530

    PEQ-4212 clear all button impliment

--- src/app/shared/psi-crud-form/psi-crud-form.component.ts
+++ src/app/shared/psi-crud-form/psi-crud-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { SimpleModalService } from 'ngx-simple-modal';
+import { ConfirmationModalComponent } from 'src/app/components/organism/confirmation-modal/confirmation-modal.component';
+
 @Component({
     selector: 'app-psi-crud-form',
     templateUrl: './psi-crud-form.component.html',
@@ -8,6 +11,7 @@ import { Router } from '@angular/router';
 export class PsiCrudFormComponent implements OnInit, OnChanges {
 
     @Input() leftHeaderTitle: string;
+    @Input() clearAll: boolean;
     @Input() rightHeaderTitle: string;
     @Input() rightHeaderBottomTitle: string;
     @Input() crudFieldConfig: any;
@@ -19,7 +23,8 @@ export class PsiCrudFormComponent implements OnInit, OnChanges {
     @Output() formSubmit = new EventEmitter<any>();
 
     constructor(
-        public router: Router
+        public router: Router,
+            private simpleModalService: SimpleModalService,
     ) { }
 
     ngOnInit(): void {
@@ -55,7 +60,64 @@ export class PsiCrudFormComponent implements OnInit, OnChanges {
         const control = this.form.get(controlName);
         return control?.invalid && this.formSubmitted;
     }
-    
+
+    /**
+     * Function of clear the form
+     * @author psi-enhancement
+     */
+    clearAllSelections(): void {
+        const hasValues = [...this.crudFieldConfig.leftSection, ...this.crudFieldConfig.rightSection].some((field) => {
+          if (field.type === 'multiselect-dropdown' && this.sellectedData[field.key]?.length > 0) {
+            return true;
+          }
+          if (field.type === 'text' && this.form?.get(field.name)?.value?.trim() !== '') {
+            return true;
+          }
+          if (field.type === 'checkbox' && this.form?.get(field.name)?.value === true) {
+            return true;
+          }
+          return false;
+        });
+      
+        if (!hasValues) {return;}
+      
+        const modalData = {
+          title: 'All data will be lost.',
+          body: 'Are you sure you wish to clear all fields?',
+          iconClass: 'fas fa-exclamation-circle error',
+          btnLabel: [
+            { type: 'Btn', label: 'No', class: 'secondary' },
+            { type: 'Btn', label: 'Yes', class: 'primary' }
+          ]
+        };
+      
+        this.simpleModalService.addModal(ConfirmationModalComponent, { modalData }).subscribe((result) => {
+          if (result.btn.label === 'Yes') {
+            [...this.crudFieldConfig.leftSection, ...this.crudFieldConfig.rightSection].forEach((field) => {
+              
+              if (field.type === 'multiselect-dropdown') {
+                this.sellectedData[field.key] = [];
+              }
+      
+              if (field.type === 'text' && this.form?.get(field.name)) {
+                this.form.get(field.name).setValue('');
+                this.form.get(field.name).markAsPristine();
+                this.form.get(field.name).markAsUntouched();
+              }
+      
+              if (field.type === 'checkbox' && this.form?.get(field.name)) {
+                this.form.get(field.name).setValue(false);
+                this.form.get(field.name).markAsPristine();
+                this.form.get(field.name).markAsUntouched();
+              }
+            });
+      
+            if (this.form) {
+              this.form.updateValueAndValidity();
+            }
+          }
+        });
+    } 
 
     /**
      * Updates the form control value based on the input change.
