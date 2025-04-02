d1a70b006ab5c532905f1a6b2a7a564f9b497702
Kris Narola <knarola@parksteet.com>
2025-04-02 15:05:49 +0530

    PEQ-2745 Redundant-Syncing

--- src/app/components/product-management/product-management-details/product-management-details.component.ts
+++ src/app/components/product-management/product-management-details/product-management-details.component.ts
@@ -104,9 +104,7 @@ export class ProductManagementDetailsComponent implements OnInit {
      */
     getSyncStatusUpdate() {
         if (this.productDetails.sync_status === 2) {
-            this.timerObj = setInterval(() => {
-                this.getSyncStatusDetails();
-            }, 30000);
+            this.initiateSyncTimerForStatusDetails();
         } else if (this.productDetails.sync_status === 3 || this.productDetails.sync_status === null) {
             this.syncStatusFail = true;
         }
@@ -153,9 +151,7 @@ export class ProductManagementDetailsComponent implements OnInit {
         this.actionButtons[0].class = 'fas fa-sync fa-spin';
         this.actionButtons[0].button = AppConstant.PRODUCT.SYNC_STATUS[2];
         this.productManagementDetailService.syncOrder(this.productDetails.id).subscribe( (result) =>{
-            this.timerObj= setInterval(() => {
-                this.getSyncStatusDetails();
-            }, 30000);
+            this.initiateSyncTimerForStatusDetails();
         });
         return true;
     }
@@ -572,6 +568,23 @@ export class ProductManagementDetailsComponent implements OnInit {
             }
             return cleanedData;
         }, {});
+    }
+
+    /**
+     * Initiates or resets the interval timer for getting sync details
+     * 
+     * @param none
+     * @returns void
+     * @author PSI-Enhancement
+     */
+    initiateSyncTimerForStatusDetails() {
+        if(this.timerObj) {
+            clearInterval(this.timerObj);
+            this.timerObj = null;
+        }
+        this.timerObj = setInterval(() => {
+            this.getSyncStatusDetails();
+        }, 30000);
     }
 }
 
