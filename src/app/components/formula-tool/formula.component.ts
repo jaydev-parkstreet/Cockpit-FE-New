48977411c1dd517a31a13d3f7f728b611145cd85
smruti-parkstreet <ssahoo@parkstreet.com>
2025-04-16 14:31:04 +0530

    COC-100229: Archive checkbox issue fixed

--- src/app/components/formula-tool/formula.component.ts
+++ src/app/components/formula-tool/formula.component.ts
@@ -421,7 +421,7 @@ export class FormulaComponent implements OnInit {
       } else if (key == 'is_active') {
         acc['active_status'] = [selectedFilters[key] == 0 ? '1' : '0'];
       } else if (key == 'is_archived') {
-        acc['is_archived'] = selectedFilters[key].length === 0 ? [0] : [1];
+        acc['is_archived'] = selectedFilters[key] ? [1] : [];
       } else {
         acc[key] = selectedFilters[key].map((item: any) => item.id);
       }
