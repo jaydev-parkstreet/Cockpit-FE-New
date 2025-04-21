51bbeb52f2b50e7f8a2fa57ec35cbb44e3a96680
smruti-parkstreet <ssahoo@parkstreet.com>
2025-04-21 11:22:45 +0530

    COC-96702: Update the extension for attachment

--- src/app/stateless/psi-upload-files/psi-upload-files.component.ts
+++ src/app/stateless/psi-upload-files/psi-upload-files.component.ts
@@ -26,7 +26,9 @@ export class PsiUploadFilesComponent implements OnInit {
      */
     onFileChange(event: any): void {
         const files: FileList = event.target.files;
-        const allowedExtensions = this.configUpload?.allowedExtensions || ['gif', 'jpeg', 'jpg', 'tiff', 'tif', 'zip', 'pdf', 'msi', 'png'];
+        const allowedExtensions = this.configUpload?.allowedExtensions || ['gif', 'jpeg', 'jpg', 'png', 'tiff', 'tif', 'zip', 'pdf',
+                                                                            'xls', 'doc', 'docx', 'xlsx',
+                                                                            'pages', 'xlsm', 'csv', 'odt'];
         const maxSize = 10 * 1024 * 1024;
         const validFiles: File[] = this.selectedFiles;
 
