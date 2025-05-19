87c41e6dd3c2a269358a8e6425540785a2db0297
manasp-parkstreet <manasp@parkstreet.com>
2025-05-19 19:18:21 +0530

    login spinner reloading issue fixed

--- src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
+++ src/app/components/authentication/psi-custom-form/psi-custom-form.component.ts
@@ -129,6 +129,8 @@ export class PsiCustomFormComponent implements OnInit, AfterViewInit {
         } catch (error) {
             this.isShowLoginErrorMsg = true;
             this.showErrorMsg = error.error.msg;
+            this.spinner.hide();
+
         }
 
     }
